2e2430b76dd1b0f4497804009c26968f7ce3d1c3
Benihin_Dev <mahendrabenihin@gmail.com>
2024-06-24 11:01:43 +0530

    sdfg

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from "./components/About.jsx";
-import Contact from "./components/Contact.jsx";
-import Services from "./components/Services.jsx";
-import Footer from "./components/Footer.jsx";
-import Home from "./components/Home.jsx";
-import Navbar from "./components/Navbar.jsx";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage.js";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
-        <Home />
-        <About />
-        <Services />
-        <Contact />
-        <Footer />
+        <Routes>
+          <Route to="/" element={<HomePage />} />
+        </Routes>
       </div>
     </Router>
   );
